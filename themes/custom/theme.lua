fe0260d1b75e0dcb3b9d57274c97bf9a807ef38e
Alexey Zankevich <zancudero@gmail.com>
2015-12-21 11:17:53 +0300

    Added planet to custom theme

--- themes/custom/theme.lua
+++ themes/custom/theme.lua
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_focus_inactive  = "~/.config/awesome/themes/cust
 theme.titlebar_maximized_button_normal_active = "~/.config/awesome/themes/custom/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "~/.config/awesome/themes/custom/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "~/.config/awesome/themes/custom/background.png"
+theme.wallpaper = "~/.config/awesome/themes/custom/planet.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "~/.config/awesome/themes/custom/layouts/fairhw.png"
