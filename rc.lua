3bba0be22b5f86a156572624bbbd4c36037d7326
Alexey Zankevich <zancudero@gmail.com>
2016-01-02 15:14:00 +0300

    removed synergy from autostart

--- rc.lua
+++ rc.lua
@@ -526,7 +526,6 @@ end
 
 -- autostart
 run_once('nm-applet')
-run_once('synergys')
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
