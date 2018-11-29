e52bd81a254d49325eea0cd868e0c255542e17e0
Alexey Zankevich <alexey.zankevich@onepeloton.com>
2018-11-29 17:12:23 -0500

    Added locking screen

--- rc.lua
+++ rc.lua
@@ -295,9 +295,10 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
-    awful.key({ modkey, }, "Print",     function () awful.util.spawn("flameshot gui")          end,
+    awful.key({ modkey, }, "Print", function () awful.util.spawn("flameshot gui") end,
+              {description = "Capture screen", group = "layout"}),
+    awful.key({ modkey, }, "d", function () awful.util.spawn("xlock -mode space") end,
               {description = "lock screen", group = "layout"}),
-    	      
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
               {description = "increase master width factor", group = "layout"}),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
